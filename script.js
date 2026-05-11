927486faf15bb640c87e17139165fe6507922692
WxarthurxW <arthur.glovaski@escola.pr.gov.br>
2026-05-11 15:23:33 +0000

    af

---
+++ script.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".botao");
+for (let i =0; 1 < botoes.length; i++){
+    botoes[i].onclick = function (){
+        for (let j=0 j<botoes.length; j++){
+        botoes[i].classList(".ativo")
+        botoes[j].classlist
+        }
+    } 
+console.log(botoes);
+}
