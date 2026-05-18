fde73f345d0453459eafb8bd0aea4ff0ed41666e
WxarthurxW <arthur.glovaski@escola.pr.gov.br>
2026-05-18 15:16:53 +0000

    .

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
 for (let i =0; 1 < botoes.length; i++){
     botoes[i].onclick = function (){
-        for (let j=0 j<botoes.length; j++){
-        botoes[i].classList(".ativo")
-        botoes[j].classlist
+        for (let j=0; j<botoes.length; j++){
+        botoes[i].classList.remove("ativo")
+        testos[j].classlist.remove("ativo")
         }
+    botoes[i].classList.add("ativo")
+    textos[i].classList.add("ativo")
     } 
 console.log(botoes);
 }
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2026-12-28T23:59:59");
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2026-12-28T23:59:59");
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2026-12-28T23:59:59");
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2026-12-28T23:59:59");
